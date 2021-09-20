3441e81d58e1d60454e7cfc09c2eb7100126c799
L'hostis <yolhostis@live.fr>
2021-09-20 17:11:05 +0200

    Issue3 fix: plus possible de renseigner des fichiers autres que jpeg jpg ou png. Test à écrire.

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,11 +1,28 @@
-import { screen } from "@testing-library/dom"
+/**
+* @jest-environment jsdom
+*/
+
+import { getByTestId, screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { readyException } from "jquery"
+import { data } from "../__mocks__/firebase"
 
+describe("Test format file in input form new Bill", () => {
+  test("format of file should be .jpg, jpeg ou .png", async () => {
+    const html = NewBillUI()
+    document.body.innerHTML = html
+    //const file = document.querySelector(`input[data-testid="file"]`)
+    //to-do write assertion
+    waitFor(() => {
+      expect(getByTestId(document.body, 'file')).toMatch(/.jpg$|.jpeg$|.png$|/);
+    })
+  })
+})
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then the form appears", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
